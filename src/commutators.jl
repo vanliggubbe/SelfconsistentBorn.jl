479e0688fd39a8f2320735dc0698379e9ac698ed
Vasilii Vadimov <vvadimov93@gmail.com>
2026-01-27 12:01:43 +0200

    Lazy adjoint commutator map

--- src/commutators.jl
+++ src/commutators.jl
@@ -15,8 +15,8 @@ Commutator(A) = Commutator(A, -1)
 
 ishermitian(A :: Commutator) = ishermitian(A.A)
 issymmetric(A :: Commutator) = issymmetric(A.A)
-Base.adjoint(A :: Commutator) = Commutator(A.A', A.sign)
-Base.transpose(A :: Commutator) = Commutator(transpose(A.A), A.sign)
+#Base.adjoint(A :: Commutator) = Commutator(A.A', A.sign)
+#Base.transpose(A :: Commutator) = Commutator(transpose(A.A), A.sign)
 Base.size(A :: Commutator) = (A.n ^ 2, A.n ^ 2)
 
 MulStyle(::Commutator) = FiveArg()
@@ -29,10 +29,46 @@ function _unsafe_mul!(y, A :: Commutator, x :: AbstractVector)
     return y
 end
 
+function _unsafe_mul!(y, B :: AdjointMap{<: Any, <: Commutator}, x :: AbstractVector)
+    A = B.lmap
+    Y = reshape(y, A.n, A.n)
+    X = reshape(x, A.n, A.n)
+    mul!(Y, A.A', X, one(eltype(X)), zero(eltype(y)))
+    mul!(Y, X, A.A', A.sign, one(eltype(y)))
+    return y
+end
+
+function _unsafe_mul!(y, B :: TransposeMap{<: Any, <: Commutator}, x :: AbstractVector)
+    A = B.lmap
+    Y = reshape(y, A.n, A.n)
+    X = reshape(x, A.n, A.n)
+    mul!(Y, transpose(A.A), X, one(eltype(X)), zero(eltype(y)))
+    mul!(Y, X, transpose(A.A), A.sign, one(eltype(y)))
+    return y
+end
+
 function _unsafe_mul!(y, A :: Commutator, x :: AbstractVector, α, β)
     Y = reshape(y, A.n, A.n)
     X = reshape(x, A.n, A.n)
     mul!(Y, A.A, X, α, β)
     mul!(Y, X, A.A, A.sign * α, one(eltype(Y)))
+    return y
+end
+
+function _unsafe_mul!(y, B :: AdjointMap{<: Any, <: Commutator}, x :: AbstractVector, α, β)
+    A = B.lmap
+    Y = reshape(y, A.n, A.n)
+    X = reshape(x, A.n, A.n)
+    mul!(Y, A.A', X, α, β)
+    mul!(Y, X, A.A', A.sign * α, one(eltype(Y)))
+    return y
+end
+
+function _unsafe_mul!(y, B :: TransposeMap{<: Any, <: Commutator}, x :: AbstractVector, α, β)
+    A = B.lmap
+    Y = reshape(y, A.n, A.n)
+    X = reshape(x, A.n, A.n)
+    mul!(Y, transpose(A.A), X, α, β)
+    mul!(Y, X, transpose(A.A), A.sign * α, one(eltype(Y)))
     return y
 end
